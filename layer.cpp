7b3304948ade409d384629be5f85fd047df1cdda
Andrew Wray <andrewwray2718@gmail.com>
2020-10-19 17:20:59 -0700

    Simplify weight initialization

--- layer.cpp
+++ layer.cpp
@@ -7,11 +7,14 @@ Layer::Layer(int numNodes, int inputSize)
 	assert(m_numNodes > 0);
 	assert(m_inputSize > 0);
 
-	// make the vector as long as needed.
+	 //make the vector as long as needed.
 	weights.reserve(m_numNodes * m_inputSize);
+	
+	// initialize to zeros
+	fill(weights.begin(), weights.end(), 0);
 }
 
-void Layer::print_weights()
+void Layer::printWeights()
 {
 	int row=0;
 	int col=0;
@@ -26,4 +29,20 @@ void Layer::print_weights()
 		std::cout << '\n';
 	}
 }
+
+double Layer::forward(std::vector<double>& inputVector)
+{ 
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 
